70f3c5ad662e1bf29fa58dd6fc4d7aa45ea60b15
sh-mihad <sh.mihad.cse@gmail.com>
2023-01-20 02:52:06 +0600

    some update

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,34 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
- 
+
     const menuItem = <>
-    <li><Link to="/">Home</Link></li>
+        <li><Link to="/">Home</Link></li>
     </>
 
 
     return (
         <div className="navbar ">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              {menuItem}
-            </ul>
-          </div>
-          <Link to="/" className="btn btn-ghost normal-case text-2xl text-red-600 ">QuadB Movies</Link>
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItem}
+                    </ul>
+                </div>
+                <Link to="/" className="btn btn-ghost normal-case text-2xl text-white ">QuadB Movies</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-white text-lg">
+                    {menuItem}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <button
+                    href="/"
+                    className="inline-flex bg-red-500 text-white items-center justify-center w-full h-12 px-6 font-semibold tracking-wide  transition duration-200 rounded shadow-md md:w-auto hover:text-deep-purple-900 bg-teal-accent-400 hover:bg-teal-accent-700 focus:shadow-outline focus:outline-none"
+                >
+                    Get Started
+                </button>
+            </div>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 text-white text-lg">
-           {menuItem}
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn bg-red-600">Get started</a>
-        </div>
-      </div>
     );
 };
 
